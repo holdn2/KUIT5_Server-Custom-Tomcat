f8ffca5537ada32f6edddf31da3fbc9c5da33792
holdn2 <yoocy01@gmail.com>
2025-04-01 14:48:58 +0900

    주석으로 코드 정리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -36,6 +36,7 @@ public class RequestHandler implements Runnable{
             String[] tokens = line.split(" ");
             if (tokens.length < 2) return;
 
+            // 요구사항 1 index.html을 반환하도록 함.
             String method = tokens[0];
             String urlPath = tokens[1];
             if (urlPath.equals("/")) {
@@ -43,31 +44,40 @@ public class RequestHandler implements Runnable{
             }
 
             // 헤더 파싱!
-            // body의 내용에 대한 length 가져오기. Content-Length 값
+            // body의 내용에 대한 length 가져오기. Content-Length 값.
             // 로그인되어있는지 쿠키 유무를 통해 확인한다.
             int contentLength = 0;
+            // 쿠키가 있을 때만 true로 바꾼다.
             boolean isLoggedIn = false;
             String headerLine;
             while (br.ready() && !(headerLine = br.readLine()).equals("")) {
+                System.out.println(headerLine);
+                // 요구사항 3번의 사용할 body를 가져오기 위해 필요함.
                 if (headerLine.startsWith("Content-Length:")) {
                     contentLength = Integer.parseInt(headerLine.split(":")[1].trim());
                 }
+                // 요구사항 6에서 헤더의 쿠키에 Cookie: logined=true 가 있을 경우 isLoggedIn을 true로 바꿔준다.
                 if (headerLine.startsWith("Cookie:")) {
-                    String[] cookies = headerLine.substring("Cookie:".length()).trim().split(";");
-                    for (String cookie : cookies) {
-                        String[] kv = cookie.trim().split("=", 2);
-                        if (kv.length == 2 && kv[0].trim().equals("logined")) {
-                            isLoggedIn = kv[1].trim().equals("true");
-                        }
+                    String cookie = headerLine.substring("Cookie:".length()).trim();
+                    // 쿠키를 = 를 기준으로 나누어 저장한다. logined와 true가 나누어진다.
+                    String[] loginCookie = cookie.split("=", 2);
+                    if (loginCookie.length == 2 && loginCookie[0].trim().equals("logined")) {
+                        // logined 뒤에 true일 때만 isLoggedIn이 true가 된다.
+                        isLoggedIn = loginCookie[1].trim().equals("true");
                     }
                 }
+
             }
 
-            // 요구사항 2번
+            // 요구사항 2번. GET 방식으로 회원가입하기. form.html에서 form태그의 method가 get일 때
             if (method.equals("GET") && urlPath.startsWith("/user/signup")) {
-                String[] pathSplit = urlPath.split("\\?", 1);
+                // split을 통해 ?를 기준으로 경로와 쿼리를 나눈다.
+                String[] pathSplit = urlPath.split("\\?", 2);
                 if (pathSplit.length > 1) {
+                    // 쿼리만 넘겨줄 것.
                     String queryString = pathSplit[1];
+                    // parseQueryParameter를 통해 &를 기준으로 파라미터를 구분하고
+                    // =를 기준으로 key와 value를 구분하여 Map에 저장한다.
                     Map<String, String> params = HttpRequestUtils.parseQueryParameter(queryString);
                     User user = new User(
                             params.get("userId"),
@@ -81,12 +91,16 @@ public class RequestHandler implements Runnable{
                 return;
             }
 
-            // 요구사항 3번
+            // 요구사항 3번. form.html에서 form태그의 method가 post일 때
             if(method.equals("POST")&&urlPath.startsWith("/user/signup")) {
+                // IOUtils.readData를 통해 요청에서 body 부분만 가져온다.
                 String body = IOUtils.readData(br, contentLength);
-                System.out.println(body);
+                System.out.println("바디 : " + body);
+
                 if (!body.isEmpty()) {
                     String queryString = body;
+                    // parseQueryParameter를 통해 &를 기준으로 파라미터를 구분하고
+                    // =를 기준으로 key와 value를 구분하여 Map에 저장한다.
                     Map<String, String> params = HttpRequestUtils.parseQueryParameter(queryString);
                     User user = new User(
                             params.get("userId"),
@@ -116,6 +130,7 @@ public class RequestHandler implements Runnable{
                 if (user != null && user.getPassword().equals(password)) {
                     response302HeaderWithCookie(dos, "/index.html", "logined=true");
                 } else {
+                    // 조건에 맞지 않으면 login_failed.html로 리다이렉트 시킨다.
                     response302Header(dos, "/user/login_failed.html");
                 }
                 return;
@@ -124,6 +139,7 @@ public class RequestHandler implements Runnable{
             // 요구사항 6
             // 사용자 목록 출력 (요구사항 6)
             if (method.equals("GET") && urlPath.equals("/user/userList")) {
+                // 쿠키에 logined=true가 없다면 login.html로 리다이렉트 시킨다.
                 if (!isLoggedIn) {
                     response302Header(dos, "/user/login.html");
                     return;
@@ -141,6 +157,8 @@ public class RequestHandler implements Runnable{
                 return;
             }
 
+            // 특정 경로에 대한 요청에 별도 처리가 없을 때 실행된다.
+            // 위 요청들에 해당되지 않으면 실행된다.
             File file = new File("webapp" + urlPath);
             System.out.println("요청한 파일 경로: " + file.getAbsolutePath());
 
@@ -158,6 +176,7 @@ public class RequestHandler implements Runnable{
         }
     }
 
+    // 200 : 요청이 성공적으로 처리됐을 때
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String urlPath) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
@@ -175,6 +194,8 @@ public class RequestHandler implements Runnable{
         return "text/html";
     }
 
+    // 302 : 요청을 처리하고 클라이언트를 다른 url로 이동시키고 싶을 때 사용한다.
+    // 브라우저는 Location: 을 보고 해당 url로 자동 이동시킨다.
     private void response302Header(DataOutputStream dos, String path) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found\r\n");
@@ -184,7 +205,7 @@ public class RequestHandler implements Runnable{
             log.log(Level.SEVERE, e.getMessage());
         }
     }
-
+    // 로그인 시 쿠키를 헤더에 추가하여 index.html로 리다이렉트시킨다.
     private void response302HeaderWithCookie(DataOutputStream dos, String path, String cookie) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found\r\n");
